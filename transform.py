3d4741a5c4c04fe0c12a1a0f5e0783393fcac43b
guokun <guokun@vanxum.com>
2018-12-17 10:16:10 +0800

    add a function to generate dct zigzag scan sequence

--- transform.py
+++ transform.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy.fftpack import dct, idct
 
 def dct2(a):
@@ -5,3 +6,28 @@ def dct2(a):
 
 def idct2(a):
     return idct( idct( a, axis=0 , norm='ortho'), axis=1 , norm='ortho')
+
+def zigzag(N):
+    try:
+        zz_mtx = np.zeros((N,N))
+    except TypeError as err:
+        print("N must be an integer, error msg: {0}".format(err))
+    except ValueError as err:
+        print("N must be larger than 0, error msg: {0}".format(err))
+    finally:
+        return -1
+    for n in range(2,N+2):
+        zz_mtx[n-2, 0] = 1/2*((-1)**n)*(n+((-1)**n)*((n-2)*n+2)-2)
+    for i in range(1, N+1):
+        for j in range(2, N+1):
+            if ((i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1)):
+                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-2] + (2*i-1)
+            else:
+                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-2] + (j-1)*2
+    zz_mtx = np.fliplr(zz_mtx)
+    for i in range(2, N+1):
+        for j in range(1, N):
+            if i > j:
+                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-1] - (i - j) ** 2
+    zz_mtx = np.fliplr(zz_mtx)    
+    return zz_mtx
